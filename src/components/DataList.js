1456dd26cfe9f7c0e5229a79e24310a27c7ad8b6
Radomyr Horban <horban.radomyr@gmail.com>
2022-12-20 19:52:23 +0200

    Removed comments

--- src/components/DataList.js
+++ src/components/DataList.js
@@ -1,29 +1,12 @@
-import {ErrorBoundary, useErrorHandler} from 'react-error-boundary';
+import {ErrorBoundary} from 'react-error-boundary';
 import DataItem from './DataItem';
 // import DataReceiverWithBoundary from './DataReceiverWithBoundary';
 import ErrorFallback from './ErrorFallback';
 
 function DataList() {
-  const handleError = useErrorHandler();
   return (
     <>
       <div>
-        {/* <ErrorBoundary FallbackComponent={ErrorFallback}>
-          <DataItem></DataItem>
-        </ErrorBoundary>
-        <ErrorBoundary FallbackComponent={ErrorFallback}>
-          <DataItem></DataItem>
-        </ErrorBoundary>
-        <ErrorBoundary FallbackComponent={ErrorFallback}>
-          <DataItem></DataItem>
-        </ErrorBoundary> */}
-
-        {/* {Array.apply(null, {length: 3}).map((e, i) => (
-          <ErrorBoundary FallbackComponent={ErrorFallback}>
-            <DataItem></DataItem>
-          </ErrorBoundary>
-        ))} */}
-
         {Array.apply(null, {length: 3}).map((e, i) => (
           <ErrorBoundary FallbackComponent={ErrorFallback}>
             <DataItem></DataItem>
