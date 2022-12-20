22df47c6b268c00ce9d69e2fea0cb6bbaf08a182
Radomyr Horban <horban.radomyr@gmail.com>
2022-12-20 20:52:03 +0200

    Updated DataReceiver.js with try/catch

--- src/components/DataReceiver.js
+++ src/components/DataReceiver.js
@@ -1,12 +1,25 @@
-import { useState } from "react";
-import { getData } from "../api";
+import {useState} from 'react';
+import {useErrorHandler} from 'react-error-boundary';
+import {getData} from '../api';
 
 function DataReceiver() {
   const [data, setData] = useState(0);
+
+  const handleError = useErrorHandler();
   return (
     <>
-      <div className="data">
-        <button onClick={() => setData(getData())}>Get new data</button>
+      <div className='data'>
+        <button
+          onClick={() => {
+            try {
+              setData(getData());
+            } catch (e) {
+              handleError(e);
+            }
+          }}
+        >
+          Get new data
+        </button>
         <div>{data}</div>
       </div>
     </>
